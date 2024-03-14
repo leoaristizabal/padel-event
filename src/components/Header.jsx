c612c1e33a33d62f2f4b8dad30df365172b5b52c
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-03-14 15:49:23 -0400

    add hamburger menu component on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Menu from './Menu';
 
 //import Link
 import {Link} from 'react-router-dom';
@@ -8,19 +9,29 @@ import Logo from '../assets/img/LogoPadelEvent.svg';
 const Header = () => {
   return(
     
-    <header className='py-6 mb-12 border-b'>
-      <div className='container mx-auto flex justify-between items-center'>
+    <header className='py-6 mb-12 border-b '>
+      <div className='relative container mx-auto flex justify-between items-center'>
         {/*logo*/}
         <Link className='w-36' to='/'>
           <img src={Logo} alt='' />
         </Link>
-  
+        
       {/*Botones de Inicio de Sesion y Sign Up */}
   
-        <div className='flex items-center gap-6'>
-          <Link className='hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> Iniciar Sesion</Link>
-          <Link className='bg-green-700 text-white px-4 py-3 rounded-lg transition' to='{/*OJO AQUIII BACKEND*/}'>Registrarse</Link>
+        <div className='hidden lg:flex items-center gap-8'>
+          
+          <Link className=' hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> Home</Link>
+          <Link className=' hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> About</Link>
+          <Link className=' hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> Buscar</Link>
+          <Link className=' hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> Contact</Link>
+
+          </div>
+
+          <div className='hidden lg:flex items-center gap-6'>
+          <Link className=' hover:text-green-900 transition' to='{/*OJO AQUIII BACKEND*/}'> Iniciar Sesion</Link>
+          <Link className=' bg-green-700 text-white px-4 py-3 rounded-lg transition' to='{/*OJO AQUIII BACKEND*/}'>Registrarse</Link>
         </div>
+      <Menu/>
       </div>
     </header>
     
---
+++ src/components/Menu.jsx
@@ -0,0 +1,88 @@
+// Menu.jsx
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="absolute top-4 right-4 transition">
+
+    <button
+        className="text-green-700
+         hover:text-green-500
+          focus:text-green-500 
+          focus:outline-none
+          transition absolute right-4"
+        onClick={toggleMenu}
+      >
+        <svg
+          className="h-6 w-6 fill-current"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {isOpen ? (
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
+            />
+          ) : (
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M4 6h16v2H4V6zm0 7h16v2H4v-2zm0 7h16v2H4v-2z"
+            />
+          )}
+        </svg>
+      </button>      
+
+
+      {isOpen && (
+        <div className="flex-row border rounded bg-green-700">
+
+          <Link
+            className="block px-4 py-2 text-white hover:bg-green-500"
+            to="{/*OJO AQUIII BACKEND*/}"
+          >
+            Home
+          </Link>
+          <Link
+            className="block px-4 py-2 text-white hover:bg-green-500"
+            to="{/*OJO AQUIII BACKEND*/}"
+          >
+            About
+          </Link>
+          <Link
+            className="block px-4 py-2 text-white hover:bg-green-500"
+            to="{/*OJO AQUIII BACKEND*/}"
+          >
+            Contact
+          </Link>
+
+          <Link
+            className="block px-4 py-2 text-white hover:bg-green-500"
+            to="{/*OJO AQUIII BACKEND*/}"
+          >
+            {" "}
+            Iniciar Sesion
+          </Link>
+          <Link
+            className="block px-4 py-2 text-white hover:bg-green-500"
+            to="{/*OJO AQUIII BACKEND*/}"
+          >
+            Registrarse
+          </Link>
+        </div>
+      )}
+
+      
+    </div>
+  );
+};
+
+export default Menu;
